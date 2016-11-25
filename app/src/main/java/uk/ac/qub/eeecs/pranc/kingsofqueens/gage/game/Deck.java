2114c9f2492ce69ea7782278ba341eae8b5cc568
Carl McCusker <Carl McCusker>
2016-11-25 10:48:30 +0000

    Initial commit of card class

--- app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/game/Deck.java
+++ app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/game/Deck.java
@@ -1,52 +1,55 @@
 package uk.ac.qub.eeecs.pranc.kingsofqueens.gage.game;
-
 import android.content.res.AssetManager;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-
 import uk.ac.qub.eeecs.pranc.kingsofqueens.R;
 import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.Game;
 import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.engine.io.AssetStore;
-
 public class Deck {
-
-
     private List<String> deckType;
     public Queue<Card> Deck = new LinkedList<Card>();
     public boolean deckIsEmpety = true;
-
+    Card [] Deck1 , Deck2, NeutralDeck;
     public Deck(){
         //setDeck(Deck1,Deck2);
     }
-
     // TODO: 20/11/2016  Update this for a Card Array
     public boolean loadDecksIntoAssestManger(Game game, String pDeck1, String pDeck2){
+        game.getAssetManager().loadAndAddJson("Neutral","Decks/Neutral.json");
         return  game.getAssetManager().loadAndAddJson(pDeck1,"Decks/"+pDeck1+".json") &&
                 game.getAssetManager().loadAndAddJson(pDeck2,"Decks/"+pDeck2+".json");
     }
-
-    public void setDeck(Card[] Deck1, Card[] Deck2){
+    public void setDeck(){
         for (Card s: Deck1) {
-            Deck.add(s);
+            if(s.inDeck == true) {
+                for (int i = 0; i < 3; i++) {
+                    Deck.add(s);
+                }
+            }
         }
         for (Card s: Deck2) {
-            Deck.add(s);
+            if(s.inDeck == true) {
+                for (int i = 0; i < 3; i++) {
+                    Deck.add(s);
+                }
+            }
+        }
+        for(Card s: NeutralDeck){
+            if(s.inDeck == true) {
+                for (int i = 0; i < 2; i++) {
+                    Deck.add(s);
+                }
+            }
         }
         deckIsEmpety = Deck.peek() == null;
-        if(!deckIsEmpety){
-        }
     }
-
     public Card [] drawCards(int draws){
-
         Card [] hand = new Card[draws];
         for(int i = 0; i < draws; i++){
             if(Deck.isEmpty()){
@@ -57,7 +60,15 @@ public class Deck {
         }
         return hand;
     }
-
+    public boolean setDeckUp(AssetStore assetStore, String DeckName1, String DeckName2){
+        Deck1 = jsonToCardCollection(assetStore, DeckName1);
+        Deck2 = jsonToCardCollection(assetStore, DeckName2);
+        NeutralDeck = jsonToCardCollection(assetStore, "Neutral");
+        if (Deck1 == null && Deck2 == null)
+            return false;
+        setDeck();
+        return deckIsEmpety;
+    }
     public Card[] jsonToCardCollection(AssetStore assetStore, String JsonFileName){
         JSONArray jsonArray = assetStore.getJson(JsonFileName);
         Card [] deck = new Card[jsonArray.length()];
@@ -72,16 +83,15 @@ public class Deck {
                 String imgFile  = object.getString("picture");
                 int EV          = object.getInt("ev");
                 boolean inDeck  = object.getBoolean("inDeck");
-                deck[index] = new Card(id,name,attack,defense,ability,imgFile,EV,inDeck);
+                String type     = object.getString("type");
+                deck[index] = new Card(id,type,defense,attack,EV,imgFile);
             }
         }catch(JSONException e){
             // TODO: Exception
             String p = e.toString();
         }
-
         return deck;
     }
-
     public int getSize() {
         return Deck.size();
     }
