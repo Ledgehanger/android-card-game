666d4a46c14e4cb022c08e76c668431f7b5267fb
Mark <markmc93@hotmail.com>
2017-04-10 14:56:16 +0100

    Fixing up Default ability

--- app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/Abilities/Default.java
+++ app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/Abilities/Default.java
@@ -1,14 +1,11 @@
 package uk.ac.qub.eeecs.pranc.kingsofqueens.gage.Abilities;
 
-import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.game.Player;
 
 /**
  * Created by mark on 09/04/2017.
  */
 
 public class Default extends NoPlayerAbility {
-
-  public  void effect(Player effectedPlayer, String effectLevel){}
   public Default(){
     hasAbility = false;
   }
