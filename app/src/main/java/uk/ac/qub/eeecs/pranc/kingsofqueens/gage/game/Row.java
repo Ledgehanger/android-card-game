08d5013322a7593fd6aaf3b7068f44d7af8e538c
Paddy_Lenovo <pdoherty1001@qub.ac.uk>
2017-04-11 15:32:09 +0000

    Preliminary Complete Field Class

--- app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/game/Row.java
+++ app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/game/Row.java
@@ -6,8 +6,11 @@ import android.graphics.Rect;
 import java.util.ArrayList;
 import java.util.List;
 
+import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.engine.graphics.IGraphics2D;
 import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.engine.input.TouchEvent;
+import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.engine.io.AssetStore;
 import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.engine.io.ElapsedTime;
+import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.genAlgorithm;
 
 /**
  * Created by Paddy_Lenovo on 11/04/2017.
@@ -17,8 +20,9 @@ public class Row {
 
     private ArrayList<Spot> myRow;
     private Bitmap rowBitmap;
-    private Rect myRowRect;
-    public final int ROWSIZE = 1;
+    private Rect rowRect;
+    public final String ROW_IMAGE_FILE = "Row";
+    public final int SPOTS_PER_ROW = 5;
 
     public Row(){
         myRow = new ArrayList<Spot>();
@@ -42,7 +46,7 @@ public class Row {
     }
 
     public Rect getMyRowRect() {
-        return myRowRect;
+        return rowRect;
     }
 
     public Boolean checkSpotPlayable(int index){
@@ -56,7 +60,7 @@ public class Row {
 
     public void update(ElapsedTime elapsedTime, List<TouchEvent> touchEvents, Hand pHand) {
         TouchEvent touchEvent = touchEvents.get(0);
-        for (int i = 0; i < ROWSIZE; i++) {
+        for (int i = 0; i < SPOTS_PER_ROW; i++) {
             Spot c = myRow.get(i);
             if (c.getCardPlaced()) {
                 if (c.getSpotRect().contains((int) touchEvent.x, (int) touchEvent.y) && touchEvent.type == 0) {
@@ -66,8 +70,42 @@ public class Row {
         }
     }
 
-    public void draw(){
+    public void draw (genAlgorithm.field side, IGraphics2D iGraphics2D, AssetStore
+            aStore){
+
+        float top;
+        float bot;
+        int left;
+        int right;
+        int topI;
+        int botI;
+
+        rowBitmap = aStore.getBitmap(ROW_IMAGE_FILE);
+        bot = iGraphics2D.getSurfaceHeight();
+        left =  166 + 100;
+        right = iGraphics2D.getSurfaceWidth() - 250;
+
+        if (side == genAlgorithm.field.TOP) {
+            topI = 0 + (int) ((bot) - (bot / 1.5)) - 75;
+            botI = iGraphics2D.getSurfaceHeight()/2 - 5;
+            rowRect = new Rect(left, topI, right, botI);
+
+        } else {
+            top = iGraphics2D.getSurfaceHeight() / 2;
+            bot = iGraphics2D.getSurfaceHeight();
+
+            botI = (int) bot  + ((int) ((top) + (top / 4) - 155)) - 1185;
+            topI = (int) top;
+
+            rowRect = new Rect(left, topI, right, botI);
+        }
+
+
+        iGraphics2D.drawBitmap(rowBitmap,null,rowRect,null);
 
 
     }
+
+
 }
+
