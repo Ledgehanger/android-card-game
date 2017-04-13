2ad2ef35b747018ec77333183ffdfdaec66bb04f
atahir01@qub.ac.uk <atahir01@qub.ac.uk>
2017-04-13 17:55:27 +0100

    GameOverScreen - To do - Create scoreBoard for score - Save to text file for a HighScoreScreen

---
+++ app/src/main/java/uk/ac/qub/eeecs/pranc/kingsofqueens/gage/game/SplashScreen.java
@@ -0,0 +1,31 @@
+package uk.ac.qub.eeecs.pranc.kingsofqueens.gage.game;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.ac.qub.eeecs.pranc.kingsofqueens.R;
+import uk.ac.qub.eeecs.pranc.kingsofqueens.gage.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //TIME TO DISPLAY SPLASH
+    private static int SPLAH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        },SPLAH_TIME_OUT);
+    }
+}
