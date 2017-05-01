7caae86909912d45cd06dc1f26263e9c96932122
40177285 <mmcaleese06@qub.ac.uk>
2017-05-01 15:35:40 +0100

    Deck Selection test added

--- app/src/androidTest/java/uk/ac/qub/eeecs/pranc/kingsofqueens/DeckSelection_Test.java
+++ app/src/androidTest/java/uk/ac/qub/eeecs/pranc/kingsofqueens/DeckSelection_Test.java
@@ -1,6 +1,7 @@
 package uk.ac.qub.eeecs.pranc.kingsofqueens;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -39,6 +40,7 @@ public class DeckSelection_Test {
         assertEquals(true,as.loadAndAddJson("Decks", "Decks/deckTypes.json"));
     }
 
+    @Test
     public void testThatAllKnownDecksLoaded() throws Exception{
         setUpDeck();
         ArrayList<String> names = new ArrayList<String>();
@@ -55,7 +57,33 @@ public class DeckSelection_Test {
         }
         assertEquals(deckNames.length, total);
 
+
+
     }
+    @Test
+    public void getPath()throws Exception{
+        setUpDeck();
+        DeckSelection[] newDecks = as.jsonToDeckCollection("Decks");
+        //Based on json layout
+        assertEquals("Decks/Psych.json"      ,newDecks[0].getPath());
+        assertEquals("Decks/Engineering.json",newDecks[1].getPath());
+        assertEquals("Decks/Theology.json"   ,newDecks[2].getPath());
+        assertEquals("Decks/Medical.json"    ,newDecks[3].getPath());
+        assertEquals("Decks/CS.json"         ,newDecks[4].getPath());
+        assertEquals("Decks/Law.json"        ,newDecks[5].getPath());
+
+    }
+    @Test
+    public void setAndGetButton()throws Exception{
+        setUpDeck();
+        DeckSelection[] newDecks = as.jsonToDeckCollection("Decks");
+
+        Rect test = new Rect(10,20,30,40);
+        newDecks[0].setButton(test);
+
+        assertEquals(test,newDecks[0].getButton());
+
+}
 
 
 }
